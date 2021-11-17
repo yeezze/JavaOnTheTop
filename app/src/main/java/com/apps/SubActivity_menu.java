880f0cdeaa115acd888cfba3691c85229eacd393
yeezze <vnqvnq11@naver.com>
2021-11-17 14:36:58 +0900

    # 서브 메뉴 화면
    - 레이아웃 완성
    - 재료 아이콘 클릭시 쿠팡 링크 이동
    - 유튜브 버튼 클릭시 이동

--- app/src/main/java/com/apps/SubActivity_menu.java
+++ app/src/main/java/com/apps/SubActivity_menu.java
@@ -4,21 +4,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SubActivity_menu extends AppCompatActivity {
 
-    Button btncom;
     ImageButton backbtn;
-    EditText edtcomment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,4 +37,29 @@ public class SubActivity_menu extends AppCompatActivity {
         });
     }
 
+    public void onBtnYoutubeClicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/ffuakdFmuh4"));
+        startActivity(intent);
+    }
+
+    public void onitembtn1Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=된장&channel=user"));
+        startActivity(intent);
+    }
+    public void onitembtn2Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=간마늘&channel=user"));
+        startActivity(intent);
+    }
+    public void onitembtn3Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=양파&channel=user"));
+        startActivity(intent);
+    }
+    public void onitembtn4Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=대파&channel=user"));
+        startActivity(intent);
+    }
+    public void onitembtn5Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=청양고추&channel=user"));
+        startActivity(intent);
+    }
 }
